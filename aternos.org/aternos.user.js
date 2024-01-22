5d07a962b46e5c6cbeda5b5b0a85ae62e8eeacf2
m00ny <danila.kononov.official@gmail.com>
2024-01-22 13:35:54 +0300

    initial commit

---
+++ aternos.org/aternos.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         Aternos Auto-Skip Anti-Adblock
+// @namespace    m00ny's Scripts
+// @version      1.0.0
+// @description  Automates skipping anti-adblock warning on aternos.
+// @author       m00ny
+// @match        https://aternos.org/*
+// @icon         https://aternos.org/favicon.ico
+// @grant        none
+// @run-at       document-end
+// @license      MIT
+// @supportURL  https://github.com/i-Moony/userscripts/issues
+// ==/UserScript==
+
+// Features:
+// - Clicks "Continue with adblocker anyway" for you...
+
+function skip() {
+    try {
+        const elements = document.querySelectorAll(".btn.btn-white");
+
+        for (const element of elements)
+            if (element.textContent.toLowerCase().includes("continue with adblocker anyway"))
+                element.click()
+    } catch (e) {
+        console.error(`[Aternos Auto-Skip] There was an error: `, e);
+    }
+}
+
+skip()
